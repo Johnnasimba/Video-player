b6ba840ef9fd149b55a0b2f5d90239ee6ebe6be7
JOHN NASIMBA <nasimba4john@gmail.com>
2021-01-05 22:40:22 +0200

    Creating play and pause functionality

--- script.js
+++ script.js
@@ -12,7 +12,25 @@ const duration = document.querySelector('.time-duration');
 const fullscreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
-
+function showPlayIcon() {
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+  }
+  
+  function togglePlay() {
+    if (video.paused) {
+      video.play();
+      playBtn.classList.replace('fa-play', 'fa-pause');
+      playBtn.setAttribute('title', 'Pause');
+    } else {
+      video.pause();
+      showPlayIcon();
+    }
+  }
+  
+  // On video end, show play button icon
+  video.addEventListener('ended', showPlayIcon);
+  
 
 
 // Progress Bar ---------------------------------- //
